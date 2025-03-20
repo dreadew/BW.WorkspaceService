d9398d970e06cf8aa18aeb8495668e95541208f2
Dreadew <p74ur@yandex.ru>
2025-03-20 19:45:08 +0300

    added new migrations

---
+++ src/WorkspaceService.Infrastructure/Migrations/20250320131833_Migration20250320.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkspaceService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration20250320 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
